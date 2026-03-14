066ac02a6e6461979324ff6b4f4271d085f32c4c
Harshit-bhardwaj08 <harshitbhardwaj845@gmail.com>
2026-03-14 13:39:30 +0530

    fix

--- backend/main.py
+++ backend/main.py
@@ -171,7 +171,7 @@ origins = [
     "http://localhost",
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    "https://threatbrief.vercel.app",
+    "https://threat-brief.vercel.app",
 ]
 
 app.add_middleware(
